1dea0525f73b000adf94bfc19d6aad74be55f41b
Hungp <hungphamduy456654@gmail.com>
2026-03-18 18:15:30 +0700

    Bai_2-Pham_DUY_Hung-2415053122116

---
+++ Bai_2/Bai_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> danhSachSoNguyen = new List<int>();
+
+            Console.WriteLine("nhap 5 so nguyen vao list");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"nhap so thu {i + 1} vao list");
+                int n = int.Parse(Console.ReadLine());
+                danhSachSoNguyen.Add(n);
+            }
+            int tong = danhSachSoNguyen.Sum();
+            Console.WriteLine($"Tong cac so: {tong}");
+            Console.ReadLine();
+        }
+    }
+}
