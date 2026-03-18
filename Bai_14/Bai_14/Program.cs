8d43003ea47f22f6234843de42c20d337f634ff8
Hungp <hungphamduy456654@gmail.com>
2026-03-18 19:16:01 +0700

    Bai_14-Pham_Duy_Hung-2415053122116

---
+++ Bai_14/Bai_14/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_1
+{
+class Student
+{
+    public int Id;
+    public string Name;
+
+    public Student(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> dsSinhVien = new List<Student>
+            {
+                new Student(2415, "Pham Duy Hung"),
+                new Student(1002, "Nguyen Van An"),
+                new Student(1003, "Tran Thi Hoa")
+            };
+        Console.WriteLine("Danh sach hien tai:");
+        foreach (var st in dsSinhVien) Console.WriteLine($"- ID: {st.Id} | Ten: {st.Name}");
+
+        Console.Write("\nNhap ID sinh vien can xoa: ");
+        if (int.TryParse(Console.ReadLine(), out int idCanXoa))
+        {
+            int soLuongDaXoa = dsSinhVien.RemoveAll(s => s.Id == idCanXoa);
+
+            if (soLuongDaXoa > 0)
+            {
+                Console.WriteLine($"\nDa xoa sinh vien co ID {idCanXoa} thanh cong!");
+            }
+            else
+            {
+                Console.WriteLine($"\nKhong tim thay sinh vien co ID {idCanXoa}.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("ID phai la mot so nguyen!");
+        }
+
+        // 3. In lại danh sách sau khi xóa
+        Console.WriteLine("\nDanh sach sau khi cap nhat:");
+        foreach (var st in dsSinhVien) Console.WriteLine($"- ID: {st.Id} | Ten: {st.Name}");
+
+        Console.ReadKey();
+    }
+}
+}
