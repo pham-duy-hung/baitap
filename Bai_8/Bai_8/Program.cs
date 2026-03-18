1a25af85c7a558d4148b021d719f6d25605a537b
Hungp <hungphamduy456654@gmail.com>
2026-03-18 18:55:58 +0700

    Bai_8-Pham_Duy_Hung-2415053122116

---
+++ Bai_8/Bai_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+namespace GenericCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> danhSach = new List<int> { 1, 3, 5, 7, 9 };
+
+            Console.WriteLine("Danh sach ban dau: " + string.Join(", ", danhSach));
+
+            var danhSachDaoNguoc = danhSach.AsEnumerable().Reverse().ToList();
+
+            Console.WriteLine("Danh sach sau khi dao nguoc: " + string.Join(", ", danhSachDaoNguoc));
+            Console.WriteLine("Danh sach goc van giu nguyen: " + string.Join(", ", danhSach));
+
+            Console.ReadLine();
+        }
+    }
+}
