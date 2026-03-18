e71c75ef2f0b502c8a102cfe486d2f804ea13006
Hungp <hungphamduy456654@gmail.com>
2026-03-18 18:41:08 +0700

    Bai_5-Pham_Duy_Hung-2415053122116

---
+++ Bai_5/Bai_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dsSinhVien = new List<string> { "Hung", "Lan", "Nam", "Hoa", "Binh" };
+
+            Console.WriteLine("Danh sach sinh vien hien tai:");
+            Console.WriteLine(string.Join(", ", dsSinhVien));
+
+            Console.Write("\nNhap ten sinh vien muon xoa: ");
+            string tenCanXoa = Console.ReadLine();
+            bool daXoa = dsSinhVien.Remove(tenCanXoa);
+
+            if (daXoa)
+            {
+                Console.WriteLine($"\nDa xoa sinh vien '{tenCanXoa}' thanh cong!");
+            }
+            else
+            {
+                Console.WriteLine($"\nKhong tim thay sinh vien '{tenCanXoa}' trong danh sach.");
+            }
+
+            Console.WriteLine("Danh sach sau khi cap nhat:");
+            Console.WriteLine(string.Join(", ", dsSinhVien));
+
+            Console.ReadLine();
+        }
+    }
+}
