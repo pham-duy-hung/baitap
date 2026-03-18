61fd1e8d91514e91a0dc55cc4b31a72d61b1eaa7
Hungp <hungphamduy456654@gmail.com>
2026-03-18 19:13:30 +0700

    Bai_13-Pham_Duy_Hung-2415053122116

---
+++ Bai_13/Bai_13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_13
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+
+        public Student(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> dsSinhVien = new List<Student>
+            {
+                new Student(2415, "Pham Duy Hung"),
+                new Student(1002, "Nguyen Van An"),
+                new Student(1003, "Tran Thi Hoa")
+            };
+
+             int tongSo = dsSinhVien.Count(); 
+
+            Console.WriteLine($"Tong so sinh vien hien co: {tongSo}");
+
+            Console.WriteLine("\nDanh sach chi tiet:");
+            foreach (var st in dsSinhVien)
+            {
+                Console.WriteLine($"- {st.Name} (ID: {st.Id})");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
