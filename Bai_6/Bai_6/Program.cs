88ac53adf71c16190b7527a963164f4aa590da81
Hungp <hungphamduy456654@gmail.com>
2026-03-18 18:46:56 +0700

    Bai_6-Pham_DUY_Hung-2415053122116

---
+++ Bai_6/Bai_6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // Cần thiết để dùng .Any()
+
+namespace GenericCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> danhSachPhim = new List<string> { "Iron Man", "Spider Man", "Thor", "Batman", "Joker" };
+
+            Console.WriteLine("Danh sach hien co: " + string.Join(", ", danhSachPhim));
+
+            Console.Write("\nNhap ten phim ban muon tim: ");
+            string timKiem = Console.ReadLine();
+
+            bool tonTai = danhSachPhim.Any(x => x.Equals(timKiem, StringComparison.OrdinalIgnoreCase));
+
+            if (tonTai)
+            {
+                Console.WriteLine($"Ket qua: Chuoi '{timKiem}' CO ton tai trong danh sach.");
+            }
+            else
+            {
+                Console.WriteLine($"Ket qua: Chuoi '{timKiem}' KHONG ton tai.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
