243f480b9f6d352f5b98c36bdd2c5bc9ab74bcd8
Hungp <hungphamduy456654@gmail.com>
2026-03-18 17:38:39 +0700

    bai_1-Pham_Duy_Hung-2415053122116

---
+++ Bai_1/BAI_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> danhsachsonguyen = new List<int>();
+
+            danhsachsonguyen.Add(19);
+            danhsachsonguyen.Add(11);
+            danhsachsonguyen.Add(123);
+            danhsachsonguyen.Add(5324);
+            danhsachsonguyen.Add(4234);
+
+            Console.WriteLine("danh sach cac so nguyen da them");
+            foreach (int so in danhsachsonguyen)
+            {
+                Console.WriteLine(so);
+            }
+            Console.ReadLine();
+        }
+    }
+}
